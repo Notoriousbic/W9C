8ba856950813d77eca92bd30e51c597fefc52375
MacPro <macpro@MacPros-MacBook-Pro.local>
2020-08-20 21:48:02 -0600

    W9C

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var tweet = ['hey', 'how are you', 'what a nice day', 'oh thats amazing lol!', 'how was your day?', 'epstein didnt kill himself', 'duh he was obviously mossad just like his buddy Wexner', 'I know that was unrelated but lemme tell you one more tidbit', 'wanna know a secret? the most influential man of the past nearly 100 years is someoene you might not know:ROY COHN!' ,'COHN has been dead 30 years and there are still people terrified of the man'];
+var counter = 0;
+
+while (counter < 10) {
+    console.log(tweet[counter])
+    counter ++;
+}
